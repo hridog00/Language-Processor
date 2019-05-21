c6c9083bcaac211c0c0c44588da9ec85af21b915
helena.ridocci <helena.ridocci@drotium.com>
2019-05-21 18:51:49 +0200

    gestion de tipos en variables

--- src/Sentencia.java
+++ src/Sentencia.java
@@ -2,9 +2,11 @@
 public class Sentencia {
 	String codigo;
 	String resultado;
+	String tipo;
 	public Sentencia(String codigo, String resultado){
 		this.codigo = codigo;
 		this.resultado = resultado;
+	
 	 }
 	
 }
